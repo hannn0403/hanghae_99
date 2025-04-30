c3208fa1cbe585748608896cfa4152cd3ce03d88
Juhyuk Han <35449467+hannn0403@users.noreply.github.com>
2025-05-01 08:41:45 +0900

    Create 17298_오큰수.py

---
+++ [BOJ]/Stack/17298_오큰수.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+ans = [0] * n 
+A = list(map(int, input().split()))
+myStack = [] 
+
+for i in range(n): 
+
+    while myStack and A[myStack[-1]] < A[i]: 
+        ans[myStack.pop()] = A[i]
+    myStack.append(i) 
+
+while myStack: 
+    ans[myStack.pop()] = -1 
+
+for i in range(n): 
+    print(ans[i], end=' ')
+
