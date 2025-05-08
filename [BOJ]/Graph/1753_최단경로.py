c1b7894d4f735f9cc50df424ed5be75aea452c09
Juhyuk Han <35449467+hannn0403@users.noreply.github.com>
2025-05-08 17:10:38 +0900

    Update 1753_최단경로.py

--- [BOJ]/Graph/1753_최단경로.py
+++ [BOJ]/Graph/1753_최단경로.py
@@ -5,39 +5,33 @@ input = sys.stdin.readline
 INF = float("inf")
 
 V, E = map(int, input().split())
-
-distance = [10000000] * (V + 1)
 K = int(input())
-distance[K] = 0
 
 adj_list = [[] for _ in range(V + 1)]
+
 for _ in range(E):
     u, v, w = map(int, input().split())
     adj_list[u].append((v, w))
+    # adj_list[v].append((u, w))
 
-visited = [False] * (V + 1)
+distance = [INF] * (V + 1)
+distance[K] = 0
 
-q = PriorityQueue()
-q.put((0, K))
+pq = PriorityQueue()
+pq.put((distance[K], K))
 
-curr_node = K
-while q.qsize() > 0:
-    curr_node = q.get()
-    c_v = curr_node[1]
-    if visited[c_v]:
-        continue
-    visited[c_v] = True
-    # print(f"c_v : {c_v}")
+while pq.qsize() > 0:
+    dist, node = pq.get()
 
-    for next_node in adj_list[c_v]:
-        node_idx, edge_dist = next_node[0], next_node[1]
-        if distance[node_idx] > distance[c_v] + edge_dist:
-            distance[node_idx] = distance[c_v] + edge_dist
-            q.put((distance[node_idx], node_idx))
+    for next in adj_list[node]:
+        next_node_idx, next_node_edge = next[0], next[1]
+        if distance[next_node_idx] > (dist + next_node_edge):
+            distance[next_node_idx] = dist + next_node_edge
+            pq.put((distance[next_node_idx], next_node_idx))
 
 
-for i in range(1, V + 1):
-    if distance[i] == 10000000:
+for i in range(1, len(distance)):
+    if distance[i] == INF:
         print("INF")
     else:
         print(distance[i])
