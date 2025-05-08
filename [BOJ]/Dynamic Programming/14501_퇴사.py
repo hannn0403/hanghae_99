6f770d2f197c950ff83e1e0512aab3a36f2acfb9
Juhyuk Han <35449467+hannn0403@users.noreply.github.com>
2025-05-08 23:14:06 +0900

    Update 14501_퇴사.py

--- [BOJ]/Dynamic Programming/14501_퇴사.py
+++ [BOJ]/Dynamic Programming/14501_퇴사.py
@@ -1,19 +1,25 @@
-import sys 
+import sys
 
-input = sys.stdin.readline 
+input = sys.stdin.readline
 
-N = int(input()) 
-D = [0] * (N + 2) 
-T = [0] * (N+1)
-P = [0] * (N+1) 
+N = int(input())
+T = [0]
+P = [0]
+for _ in range(N):
+    t, p = map(int, input().split())
+    T.append(t)
+    P.append(p)
 
-for i in range(1, N+1): 
-    T[i], P[i] = map(int, input().split())
+dp = [0] * (N + 2)
 
-for i in range(N, 0, -1): 
-    if i + T[i] > N+1: 
-        D[i] = D[i+1]
-    else: 
-        D[i] = max(D[i+1], P[i] + D[i + T[i]]) 
+for i in range(N, -1, -1):
+    t, p = T[i], P[i]
+    # 현재 해당 과제의 기간이 퇴사 전에 끝나지 않는다면 무용지물
+    if i + T[i] > N + 1:
+        dp[i] = dp[i + 1]
+        continue
 
-print(D[1])
+    dp[i] = max(dp[i + 1], p + dp[i + t])
+    # print(dp)
+
+print(dp[0])
