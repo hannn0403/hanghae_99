a5e772fa041f3484bd54317eb5cd51e2739cc73a
Juhyuk Han <35449467+hannn0403@users.noreply.github.com>
2025-05-08 20:54:10 +0900

    Update 2042_구간합구하기.py

--- [BOJ]/Tree/2042_구간합구하기.py
+++ [BOJ]/Tree/2042_구간합구하기.py
@@ -3,56 +3,63 @@ import sys
 input = sys.stdin.readline
 
 N, M, K = map(int, input().split())
-# M : 수의 변경이 일어나는 횟수
-# K : 구간의 합을 구하는 횟수
 
-arr = []
+
+def get_sum(b, c, k):
+    # 일단 b와 c를 seg_tree idx로 변경
+    start_idx = 2**k - 1 + b
+    end_idx = 2**k - 1 + c
+    selected_list = []
+    while start_idx <= end_idx:
+        if start_idx % 2 == 1:
+            selected_list.append(seg_tree[start_idx])
+        if end_idx % 2 == 0:
+            selected_list.append(seg_tree[end_idx])
+
+        start_idx = (start_idx + 1) // 2
+        end_idx = (end_idx - 1) // 2
+    # print(f"get_sum의 마지막 list : {selected_list}")
+    return sum(selected_list)
+
+
+def change_value(b, c, k):
+    # b번째 수를 c로 바꾼다.
+    # b를 세그먼트 트리 인덱스로 변경
+    b = 2**k - 1 + b
+    diff = c - seg_tree[b]
+
+    while b > 0:
+        seg_tree[b] += diff
+        if b == 1:
+            break
+        b = b // 2
+
+
+arr = [0]
 for _ in range(N):
-    arr.append(int(input()))
+    num = int(input())
+    arr.append(num)
 
-# print(f"arr : {arr}")
-
+# k 구하기
 k = 0
-while N > 2**k:
+while 2**k < N:
     k += 1
 
-tree = [0] * (2 ** (k + 1))
-for i in range(N):
-    tree[2 ** (k) + i] = arr[i]
-# print(f"tree : {tree}")
+# 세그먼트 트리 만들기
+seg_tree = [0] * (2 ** (k + 1))
+# 리프노드에 입력받은 리스트 넣기
+for i in range(1, N + 1):
+    seg_tree[2**k - 1 + i] = arr[i]
 
-for i in range(2**k - 1):
-    idx = 2**k - 1 - i
-    tree[idx] = tree[2 * idx] + tree[2 * idx + 1]
+for i in range(2**k - 1, -1, -1):
+    seg_tree[i] = seg_tree[i * 2] + seg_tree[i * 2 + 1]
 
-# print(f"tree : {tree}")
+# print(arr)
+# print(seg_tree)
 
 for _ in range(M + K):
     a, b, c = map(int, input().split())
-    if a == 1:  # b번째 수를 c로 변경
-        curr_idx = 2**k - 1 + b
-        prev_num = tree[curr_idx]
-        diff = c - prev_num
-
-        while curr_idx >= 1:
-            tree[curr_idx] = tree[curr_idx] + diff
-            # print(f"{curr_idx} 수정 : {tree}")
-            curr_idx = curr_idx // 2
-
-    elif a == 2:  # b번째 수부터 c번째 수까지의 합을 출력
-        selected = []
-        start_idx = 2**k - 1 + b
-        end_idx = 2**k - 1 + c
-
-        while start_idx <= end_idx:
-            if start_idx % 2 == 1:
-                selected.append(tree[start_idx])
-                # print(f"selected : {selected}")
-            if end_idx % 2 == 0:
-                selected.append(tree[end_idx])
-                # print(f"selected : {selected}")
-
-            start_idx = (start_idx + 1) // 2
-            end_idx = (end_idx - 1) // 2
-
-        print(sum(selected))
+    if a == 1:
+        change_value(b, c, k)
+    elif a == 2:
+        print(get_sum(b, c, k))
