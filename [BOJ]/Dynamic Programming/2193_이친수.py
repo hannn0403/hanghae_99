8aaa485f6f2d8426d7babb3283a1b68feaf67f9f
Juhyuk Han <35449467+hannn0403@users.noreply.github.com>
2025-05-08 23:21:40 +0900

    Update 2193_이친수.py

--- [BOJ]/Dynamic Programming/2193_이친수.py
+++ [BOJ]/Dynamic Programming/2193_이친수.py
@@ -3,20 +3,22 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-if N == 1 or N == 2:
+if N == 1:
+    print(1)
+    exit()
+elif N == 2:
     print(1)
     exit()
 
-dp = [0] * (N + 1)
-sum_arr = [0] * (N + 1)
-
-dp[1] = 1
-sum_arr[1] = 1
-dp[2] = 1
-sum_arr[2] = 2
+one = [0] * (N + 1)
+zero = [0] * (N + 1)
+one[1] = 1
+zero[1] = 0
+one[2] = 0
+zero[2] = 1
 
 for i in range(3, N + 1):
-    dp[i] = sum_arr[i - 2] + 1
-    sum_arr[i] = sum_arr[i - 1] + dp[i]
+    one[i] = zero[i - 1]
+    zero[i] = one[i - 1] + zero[i - 1]
 
-print(dp[-1])
+print(one[-1] + zero[-1])
